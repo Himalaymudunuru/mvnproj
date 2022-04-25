72b59621526ed84a1abe9f70593c996043352f96
babu8500 <94165987+babu8500@users.noreply.github.com>
2022-04-25 15:01:46 +0530

    Update babu.groovy

--- babu.groovy
+++ babu.groovy
@@ -1,1 +1,35 @@
 
+pipeline {
+    agent any
+
+    tools {
+        
+        maven "mvn"
+    }
+      
+    stages {
+        stage('Build') {
+            steps {
+              
+                git 'https://github.com/YB-2009/mvnproj.git'
+
+                
+                sh "mvn -Dmaven.test.failure.ignore=true clean package install"
+
+              
+            
+
+            
+            }
+        }
+    stage (deploy){
+        steps {
+            
+            sh "sudo cp /var/lib/jenkins/.m2/repository/honey/ansible/1.03/ansible-1.03.war /var/lib/tomcat8/webapps/"
+        }
+    }
+    
+    
+      }
+ 
+    }
